d60bac31870dc846b7fc5adc49107b32f25680c3
EmiKnape <enknape@aol.com>
2018-06-04 23:01:41 +0000

    automatically backed up by learn

--- about_me.rb
+++ about_me.rb
@@ -1,12 +1,4 @@
-def about_me
-  puts "my name is Emi"
-  puts "I grew up in Cave Creek"
-  puts "I am 16 years old"
-  puts "My favorite food is cheese"
-end
-250.times do
-about_me
-end
+
 puts "How are you feeling?"
 answer = gets.chomp
 puts "so you're feeling #{answer}?"
